9f6ff7e42a7a0d7efbc05c8ab59bfb648db54267
Kiran Anand <liteninkiran@hotmail.com>
2026-08-16 13:15:52 +0100

    Form model & FieldTree

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { form, FormField, FormRoot } from '@angular/forms/signals';
+
+interface LoginFormModel {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  imports: [FormRoot, FormField, CommonModule],
 })
-export class App {}
+export class App {
+  model = signal<LoginFormModel>({
+    email: '',
+    password: '',
+  });
+  loginForm = form(this.model);
+}
